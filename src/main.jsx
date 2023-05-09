8215b12f35dc9f97edefa37951be11b10d04b224
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-05-09 09:00:44 +0300

    Edite module reference TodoApp function

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import TodoApp from "./components/TodoApp";
+import TodoApp from '@/components/TodoApp';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
