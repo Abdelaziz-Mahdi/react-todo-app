a1bf7b36280d90fd701f3b458f75f5881db9af9e
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-05-09 09:53:54 +0300

    Create TodoLogic  return InputTodo and title list

---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,32 @@
+import InputTodo from '@/components/InputTodo';
+
+const TodosLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
+  return (
+    <div>
+      <InputTodo />
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TodosLogic;
