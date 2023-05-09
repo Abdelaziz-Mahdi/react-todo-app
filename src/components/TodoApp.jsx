05168acca951c37637a0b2e9b18c7d53e78cfe90
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-05-09 09:05:51 +0300

    Remove div parent TodoApp function return

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
+    
 const TodoApp = () => {
-    return (
-      <div>
-        <h1>Hello world!</h1>
-        <p>I am in a React Component!</p>
-      </div>
-    );
-  };
-  export default TodoApp;
-  
+  return (
+    <React.Fragment>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+    </React.Fragment>
+  );
+};
+export default TodoApp;
