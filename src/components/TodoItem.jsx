2ef196ae53c83767d3589ff9672bc516edffd1ad
Abdelaziz Mohamed Ali <107482707+Abdelaziz-Ali@users.noreply.github.com>
2023-07-11 20:32:58 +0300

    Updating the State Using setTodos Updater Function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,25 @@
 import PropTypes from 'prop-types';
 
 const TodoItem = ({ itemProp, setTodos }) => {
+  
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+    prevState.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    })
+  );
+  };
   return (
     <li>
-      <input type='checkbox' />
+      <input type='checkbox' 
+      checked={itemProp.completed}
+      onChange={() => handleChange(itemProp.id)}/>
       {itemProp.title}
     </li>
   );
